e9821a2a0bfe816d6d8546c323588d3b43d967d9
karthikgeek1 <kjdnskjn>
2022-06-24 11:08:16 +0530

    corrected

--- q11.js
+++ q11.js
@@ -1,19 +1,19 @@
-// how to store the n value in temp sir output is not coming
+
 function pallindrome(n,rev,temp){
-    temp=n
     if(n!=0){
         rem=n%10
         rev=10*rev+rem
-        y=Math.floor(n/10)
-        return pallindrome(y,rev,temp)
+        n=Math.floor(n/10)
+        return pallindrome(n,rev,temp)
     }
+    console.log(rev);
     if(rev==temp){
-       console.log("pallindrome")
+       return "pallindrome"
     }
-    else(
-        console.log("not a pallindrome")
-    )
+    else{
+        return "not a pallindrome"
+    }
+    
 }
-pallindrome(123,0,0)
+console.log(pallindrome(121,0,121))
 
-// please correct this program sir
